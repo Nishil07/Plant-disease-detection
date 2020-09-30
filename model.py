72475661fc2a4c5d69d9749c4784a8fe988bbed0
Nishil <63183230+Nishil07@users.noreply.github.com>
2020-09-30 21:59:15 +0530

    Update model.py

--- model.py
+++ model.py
@@ -114,4 +114,35 @@ for epoch in range(1, n_epochs+1):
             print('Epoch %d, Batch %d loss: %.16f' %
                   (epoch, batch_i + 1, train_loss / 20))
             train_loss = 0.0
+
+def test(loaders, model, criterion):
+
+    # monitor test loss and accuracy
+    test_loss = 0.
+    correct = 0.
+    total = 0.
+
+    model.eval()
+    for batch_idx, (data, target) in enumerate(loaders):
+        # move to GPU
+        # forward pass: compute predicted outputs by passing inputs to the model
+        output = model(data)
+        # calculate the loss
+        loss = criterion(output, target)
+        # update average test loss 
+        test_loss = test_loss + ((1 / (batch_idx + 1)) * (loss.data - test_loss))
+        # convert output probabilities to predicted class
+        pred = output.data.max(1, keepdim=True)[1]
+        # compare predictions to true label
+        correct += np.sum(np.squeeze(pred.eq(target.data.view_as(pred))).cpu().numpy())
+        total += data.size(0)
+            
+    print('Test Loss: {:.6f}\n'.format(test_loss))
+
+    print('\nTest Accuracy: %2d%% (%2d/%2d)' % (
+        100. * correct / total, correct, total))
+
+test(train_loader, vgg16, criterion)
+
+joblib.dump(vgg16,'model.pkl')
             
